9ba44467a0942f4d953f44bd330b03909bd4f696
rekyuu <senkan.re.kyuu@gmail.com>
2024-06-12 02:11:18 -0600

    More cleanup

--- hosts/ikuyo/configuration.nix
+++ hosts/ikuyo/configuration.nix
@@ -1,10 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, modulesPath, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
   imports =
     [
       (modulesPath + "/installer/scan/not-detected.nix")
