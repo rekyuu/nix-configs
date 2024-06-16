357b4bb5cc1c0378dd3ad8a2af51f4c08d778d19
rekyuu <senkan.re.kyuu@gmail.com>
2024-06-15 21:17:26 -0600

    nevermind lol

--- home/applications/hyprlock.nix
+++ home/applications/hyprlock.nix
@@ -1,14 +1,11 @@
-{
-  pkgs,
-  ...
-}: let
+{...}: let
   monitorL = "HDMI-A-1";
   monitorC = "DP-1";
   monitorR = "DP-2";
   
   wallpaper = builtins.path {
-    name = "wallpaper.jpg";
-    path = ../static/wallhaven-k9rqp7.jpg;
+    name = "lockscreen.png";
+    path = ../static/wallhaven-eyk71r-no-drop-shadow-darkened-blur.png;
   };
 in {
   programs.hyprlock = {
