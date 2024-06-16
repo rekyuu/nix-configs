14a03b08298dfe8a2dce34084d5ebd5d719604f3
rekyuu <senkan.re.kyuu@gmail.com>
2024-06-15 20:18:28 -0600

    Updated flake; Updated jellyfin-rpc

---
+++ home/applications/jellyfin-rpc.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  ...
+}: {
+  systemd.user.services.jellyfin-rpc = {
+    Unit = {
+      Description = "Jellyfin-RPC Service";
+      Documentation = "https://github.com/Radiicall/jellyfin-rpc";
+      After = [ "network.target" ];
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${(pkgs.callPackage ../../pkgs/jellyfin-rpc {})}/bin/jellyfin-rpc";
+      Restart = "on-failure";
+    };
+  };
+}
