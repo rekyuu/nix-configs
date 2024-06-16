5436d536fbbf1b0fd66231fda6a440d3aa0b27c9
rekyuu <senkan.re.kyuu@gmail.com>
2024-06-15 21:17:26 -0600

    nevermind lol

--- home/applications/hyprpaper.nix
+++ home/applications/hyprpaper.nix
@@ -1,14 +1,11 @@
-{
-  pkgs,
-  ...
-}: let
+{...}: let
   monitorL = "HDMI-A-1";
   monitorC = "DP-1";
   monitorR = "DP-2";
 
   wallpaper = builtins.path {
     name = "wallpaper.jpg";
-    path = ../static/wallhaven-k9rqp7.jpg;
+    path = ../static/wallhaven-zxvkmy.jpg;
   };
 in {
   services.hyprpaper = {
