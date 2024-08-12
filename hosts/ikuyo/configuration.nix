da1802d73aa20932c36f555b850050ec29c24d21
rekyuu <senkan.re.kyuu@gmail.com>
2024-08-12 04:24:52 -0600

    Added automatic nix gc

--- hosts/ikuyo/configuration.nix
+++ hosts/ikuyo/configuration.nix
@@ -177,6 +177,12 @@ in {
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };  
 
   nixpkgs = {
