d6d561a848e4edc3a1da68a0af434c2f3bb4720c
rekyuu <senkan.re.kyuu@gmail.com>
2025-06-30 12:54:08 -0600

    Add nomacs; Update ffmpeg image seq script

--- home/scripts/bin/ffmpeg-image-sequence-to-mp4.sh
+++ home/scripts/bin/ffmpeg-image-sequence-to-mp4.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
-FILENAME="$1"
-FPS="$2"
-START_FRAME="$3"
+PREFIX="$1"
+FILETYPE="$2"
+RESOLUTION="$3"
+FPS="$4"
+START_FRAME="$5"
 
-ffmpeg -r "$FPS" -f image2 -s 1280x720 -start_number "$START_FRAME" -i "$FILENAME"%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p "$FILENAME".mp4
+ffmpeg -r "$FPS" -f image2 -s "$RESOLUTION" -start_number "$START_FRAME" -i "$PREFIX"%04d."$FILETYPE" -vcodec libx264 -crf 25 -pix_fmt yuvj420p "$PREFIX"_OUT.mp4
