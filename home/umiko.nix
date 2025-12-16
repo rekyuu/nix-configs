269c541b639678ed720c6776f533e6de4c94c66b
rekyuu <senkan.re.kyuu@gmail.com>
2025-12-16 03:24:39 -0700

    Add umiko

---
+++ home/umiko.nix
@@ -0,0 +1,58 @@
+{
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./applications/btop.nix
+    ./applications/zsh.nix
+  ];
+
+  home = {
+    username = "rekyuu";
+    homeDirectory = "/home/rekyuu";
+  };
+
+  home.packages = with pkgs; [
+    comma
+    iotop
+    jq
+    lsof
+    python3
+    sl
+    (buildEnv { name = "scripts"; paths = [ ./scripts ]; })
+  ];
+
+  programs = {
+    bash.enable = true;
+
+    git = {
+      enable = true;
+      lfs.enable = true;
+      settings = {
+        user.name = "rekyuu";
+        init.defaultBranch = "main";
+        commit.gpgsign = true;
+      };
+    };
+
+    home-manager.enable = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  home.sessionPath = [ ];
+
+  home.sessionVariables = { };
+
+  services = { };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.05";
+}
