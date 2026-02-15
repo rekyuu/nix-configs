7691747e7a1e16d3ec44306df7c4004509e1e536
rekyuu <senkan.re.kyuu@gmail.com>
2026-02-15 03:21:09 -0700

    Create initial fluorite config

--- hosts/fluorite/configuration.nix
+++ hosts/fluorite/configuration.nix
@@ -1,1 +1,216 @@
-{ }
+{
+  config, 
+  lib, 
+  pkgs, 
+  modulesPath, 
+  ... 
+}: {
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    loader = {
+      raspberry-pi = {
+        firmwarePackage = pkgs.raspberrypifw;
+        bootloader = "kernel";
+      };
+    };
+
+    kernelPackages = pkgs.linuxPackages_rpi5;
+
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+
+    initrd = {
+      availableKernelModules = [ "usbhid" ];
+      kernelModules = [ ];
+    };
+  };
+
+  networking = {
+    hostName = "fluorite";
+    
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+
+    firewall = {
+      allowedTCPPorts = [ 22 ];
+      allowedUDPPorts = [ ];
+      trustedInterfaces = [ ];
+    };
+  };
+
+  time = {
+    hardwareClockInLocalTime = true;
+    timeZone = "US/Mountain";
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocales = [ "ja_JP.UTF-8/UTF-8" ];
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" ];
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_COLLATE = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  fileSystems = {
+    "/" = { 
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+
+    "/boot/firmware" = {
+      device = "/dev/disk/by-uuid/2175-794E";
+      fsType = "vfat";
+      options = [
+        "noatime"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=1min"
+      ];
+    };
+  };
+
+  swapDevices = [ ];
+
+  hardware = { 
+    # https://github.com/nvmd/nixos-raspberrypi-demo/blob/main/pi5-configtxt.nix
+    # raspberry-pi.config = {
+    #   all = { 
+    #     options = {
+    #       enable_uart = {
+    #         enable = true;
+    #         value = true;
+    #       };
+
+    #       uart_2ndstage = {
+    #         enable = true;
+    #         value = true;
+    #       };
+    #     };
+
+    #     base-dt-params = {
+    #       pciex1 = {
+    #         enable = true;
+    #         value = "on";
+    #       };
+
+    #       pciex1_gen = {
+    #         enable = true;
+    #         value = "3";
+    #       };
+    #     };
+    #   };
+    # };
+  };
+
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+      download-buffer-size = 536870912; # 512 MB
+
+      substituters = [
+        "https://cache.nixos.org?priority=10"
+        "https://anyrun.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://nix-gaming.cachix.org"
+        "https://nixos-raspberrypi.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+      ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  nixpkgs = {
+    hostPlatform = lib.mkDefault "aarch64-linux";
+    
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      bash
+      git
+      tree
+      vim
+      wget
+      zsh
+    ];
+
+    variables = {
+      EDITOR = "vim";
+      VISUAL = "vim";
+      SYSTEMD_EDITOR = "vim";
+    };
+  };
+
+  programs = {
+    mtr.enable = true;
+    zsh.enable = true;
+  };
+
+  services = {
+    gvfs.enable = true;
+
+    openssh = {
+      enable = true;
+
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        ChallengeResponseAuthentication = false;
+        MaxAuthTries = 2;
+        AllowTcpForwarding = false;
+        X11Forwarding = false;
+        AllowAgentForwarding = false;
+      };
+    };
+  };
+
+  security = {
+    sudo.wheelNeedsPassword = false;
+  };
+
+  users.users.rekyuu = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
+    shell = pkgs.zsh;
+  };
+
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
--- hosts/fluorite/rekyuu.nix
+++ hosts/fluorite/rekyuu.nix
@@ -1,1 +1,62 @@
-{ }
+{
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../common/applications/btop.nix
+    ../common/applications/zsh.nix
+  ];
+
+  zsh.promptColor = "green";
+
+  home = {
+    username = "rekyuu";
+    homeDirectory = "/home/rekyuu";
+  };
+
+  home.packages = with pkgs; [
+    comma
+    direnv
+    iotop
+    jq
+    lsof
+    nix-direnv
+    python3
+    sl
+    (buildEnv { name = "scripts"; paths = [ ../../scripts ]; })
+  ];
+
+  programs = {
+    bash.enable = true;
+
+    git = {
+      enable = true;
+      lfs.enable = true;
+      settings = {
+        user.name = "rekyuu";
+        init.defaultBranch = "main";
+        commit.gpgsign = true;
+      };
+    };
+
+    home-manager.enable = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  home.sessionPath = [ ];
+
+  home.sessionVariables = { };
+
+  services = { };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "25.11";
+}
