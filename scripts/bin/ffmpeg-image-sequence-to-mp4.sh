8faf1108f162f7426343bd5ab44ea4a80ffac749
rekyuu <senkan.re.kyuu@gmail.com>
2026-04-06 21:01:29 -0600

    Update image seq script

--- scripts/bin/ffmpeg-image-sequence-to-mp4.sh
+++ scripts/bin/ffmpeg-image-sequence-to-mp4.sh
@@ -2,8 +2,14 @@
 
 PREFIX="$1"
 FILETYPE="$2"
-RESOLUTION="$3"
-FPS="$4"
-START_FRAME="$5"
+FPS="$3"
+START_FRAME="$4"
 
-ffmpeg -r "$FPS" -f image2 -s "$RESOLUTION" -start_number "$START_FRAME" -i "$PREFIX"%04d."$FILETYPE" -vcodec libx264 -crf 25 -pix_fmt yuvj420p "$PREFIX"_OUT.mp4
+ffmpeg \
+    -r "$FPS" \
+    -f image2 \
+    -start_number "$START_FRAME" \
+    -i "$PREFIX"%04d."$FILETYPE" \
+    -vcodec libx265 \
+    -qp 0 \
+    "$PREFIX"_OUT.mp4
